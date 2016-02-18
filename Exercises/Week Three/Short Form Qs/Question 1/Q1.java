3995c4fb7425f3d965483506fd947ff394b314bb
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-02-18 20:32:46 +0000

    Exercises - Week 3 - Q1 (short form) - answered, examples to follow

--- Exercises/Week Three/Short Form Qs/Question 1/Q1.java
+++ Exercises/Week Three/Short Form Qs/Question 1/Q1.java
@@ -2,18 +2,24 @@
  * Question: Write down 3 differences between
  * abstract classes and interfaces in Java 8
  * Provide examples to illustrate answer.
+ *
+ * Answer below:
  */
 
 /**
  * Difference 1:
- *
+ * Abstract classes can declare fields that are neither static nor final,
+ * Interfaces cannot.
  */
 
 /**
  * Difference 2:
+ * Abstract classes can declare fields as public, private, protected -
+ * Interface fields are always public (and static and final).
  */
 
 /**
  * Difference 3:
- *
+ * Only one abstract class can be extended -
+ * Multiple interfaces can be implemented.
  */

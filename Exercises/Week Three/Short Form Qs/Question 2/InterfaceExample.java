e1d6c2b6f6faad9d0d5cc35580d7c85ca242de67
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-02-20 13:50:33 +0000

    Week 3 - continued - moving on, q1 a) done

---
+++ Exercises/Week Three/Short Form Qs/Question 2/InterfaceExample.java
@@ -0,0 +1,11 @@
+/**
+ * Interface for demonstration of true/false questions:
+ * Question a) no methods
+ * @author Annabel Jump
+ */
+public interface InterfaceExample {
+    int anInt = 6;
+
+    String aString = "hello";
+
+}
---
+++ Exercises/Week Three/Short Form Qs/Question 2/Q2.java
@@ -0,0 +1,15 @@
+/**
+ *True or False?
+ * a) Every interface must have at least one method
+ * b) An interface can declare instance fields that an implementing class
+ * must also declare
+ * c) Although you can't instantiate an interface, an interface definition can declare
+ * constructor methods that require an implementing class to provide constructors with
+ * given signatures.
+ *
+ * @author of answers is Annabel Jump
+ */
+
+/**
+ * a) False - see InterfaceExample.java
+ */

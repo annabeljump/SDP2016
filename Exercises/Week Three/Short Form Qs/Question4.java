76202cb7e48eb438b369086200c8d85baf5db439
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-02-21 13:06:32 +0000

    Week 3 - continued - answered q4

---
+++ Exercises/Week Three/Short Form Qs/Question4.java
@@ -0,0 +1,14 @@
+/**
+ * Question 4: What is the value of a stub class like WindowAdapter (not replicated)
+ * which is composed of methods that do nothing?
+ *
+ * This answer:
+ * @author Annabel Jump
+ */
+
+/**
+ * The value of a stub class like WindowAdapter is that it allows you to create
+ * a subclass of it and choose which methods to implement - as opposed to directly
+ * implementing the interface WindowListener, where all of the specified methods
+ * must be implemented.
+ */

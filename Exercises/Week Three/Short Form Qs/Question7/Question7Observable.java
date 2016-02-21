bb2111fe202d663397310f46d78d32640f6b836f
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-02-21 16:41:20 +0000

    Week 3 - continued - short form q7 - Observable object added

--- Exercises/Week Three/Short Form Qs/Question7/Question7Observable.java
+++ Exercises/Week Three/Short Form Qs/Question7/Question7Observable.java
@@ -6,5 +6,25 @@ import java.util.Observable;
  * Observable Objects for Question 7.
  * @author Annabel Jump
  */
-public class Question7Observable {
+public class Question7Observable extends Observable {
+
+    private Double orwell = 19.84;
+
+    //Initialise the value
+    public void ObservableValue(Double o){
+        this.orwell = o;
+    }
+
+    //Method to change the value
+    public void setOrwell(Double g){
+        this.orwell = g;
+        //Mark that the value has been changed and notify observers
+        setChanged();
+        notifyObservers();
+    }
+
+    //Method to return the (changed) value
+    public double getOrwell(){
+        return orwell;
+    }
 }

cf3efe0c73d2e3471e29cd69844fdd38ea838b87
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-02-21 12:59:28 +0000

    Week 3 - continued - answered q3 (not entirely satisfactorily but will return later)

--- Exercises/Week Three/Short Form Qs/Question3Interface.java
+++ Exercises/Week Three/Short Form Qs/Question3Interface.java
@@ -7,8 +7,18 @@
  * @author Annabel Jump
  */
 
+/*
+    Notification methods do not imply that any action be taken or values returned.
+    For example, as below, if a picture is right clicked, the method must do
+    something - but if left clicked, the method may be ignored - this is for
+    the implementing class to decide.
+    (I have specified picture here so as not to get into left clicking links or buttons)
+    */
+
 public interface Question3Interface {
 
-    //Put methods here!
+    public void rightClickPicture();
+
+    public void leftClickPicture();
 
 }

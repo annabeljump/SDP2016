bdffe206c3a614406c08374395589e778b405ab6
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-02-22 01:58:57 +0000

    Week 3 - continued - short form q9 added - not yet answered.

---
+++ Exercises/Week Three/Short Form Qs/Question9.java
@@ -0,0 +1,13 @@
+/**
+ * Question: Provide examples of two commonly used Java methods
+ * that return a new object.
+ *
+ * This answer:
+ * @author Annabel Jump
+ */
+public class Question9 {
+
+    //Not sure about this one yet - not sure what is "common".
+    //Will need to come back to it.
+
+}

ac20e18e9e029af540dbab7c3eadc809dd9b303f
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-02-23 20:55:35 +0000

    Week 3 - continued - short form q11 added, not answered yet

---
+++ Exercises/Week Three/Short Form Qs/Question11.java
@@ -0,0 +1,15 @@
+/**
+ * Question: If you want to direct output to System.out instead of to a file,
+ * you can create a Writer object that directs its output to System.out: [Example]
+ *
+ * Write a code example to define a Writer object that wraps text at 15 characters,
+ * centres the text, sets the text to random casing, and directs the output to System.out.
+ * Which design pattern are you using?
+ *
+ * This Answer:
+ * @author Annabel Jump
+ */
+
+// To follow
+public class Question11 {
+}

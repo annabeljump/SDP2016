6beda0bded33ae000aed0e77f2ecea9880787b08
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-02-23 20:59:58 +0000

    Week 3 - continued - short form q10 added (as QuestionTen - random IntelliJ problems) - not answered yet

---
+++ Exercises/Week Three/Short Form Qs/QuestionTen.java
@@ -0,0 +1,13 @@
+/**
+ * IntelliJ had some random problems with me calling the file Question10,
+ * hence QuestionTen.
+ *
+ * Question: What are the signs that a Factory Method is at work?
+ *
+ * This answer:
+ * @author Annabel Jump
+ */
+
+/**
+ * Signs a Factory Method is at work:
+ */

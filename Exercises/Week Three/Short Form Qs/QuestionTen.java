d0e676d9aaae2db332859b020ee5a075e6eec767
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-02-23 21:40:11 +0000

    Week 3 - continued - short form q10 done for now

--- Exercises/Week Three/Short Form Qs/QuestionTen.java
+++ Exercises/Week Three/Short Form Qs/QuestionTen.java
@@ -10,4 +10,12 @@
 
 /**
  * Signs a Factory Method is at work:
+ * - There is an "abstract" "creator" class/interface which specifies the supertype of object to be
+ * created by the implementing classes (this may not actually be an Abstract class).
+ * - Implementing classes will specify the exact object types, which will all be subtypes
+ * of the one in the "creator" class/interface.
+ * - This way more than one subtype of object can be created through the "creator" class, using
+ * different implementations.
+ * - The object returned through the "creator" class will be of the same parent class
+ *
  */

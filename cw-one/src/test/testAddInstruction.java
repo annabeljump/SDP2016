a8e5fa6ca400a25e92fa8714d1adc7f5bd7083cc
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-03-20 20:52:24 +0000

    CW1 - starting to write unit tests but needs thinking about

---
+++ cw-one/src/test/testAddInstruction.java
@@ -0,0 +1,22 @@
+package src.test;
+
+import src.*;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Assert.*;
+
+/**
+ * Tests for AddInstruction.
+ * @author Annabel Jump
+ */
+public class testAddInstruction {
+
+    @Before
+    public void setup() {
+        AddInstruction testee = new AddInstruction("L1", 0, 3, 4);
+
+    }
+
+}

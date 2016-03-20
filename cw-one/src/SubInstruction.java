13c2c38e2bd169e3e022576855247e385d38a9ae
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-03-20 14:11:46 +0000

    CW1 - added Subtraction instruction & edited Translator file. Added sml file to test both addition & subtraction (code testing will follow)

---
+++ cw-one/src/SubInstruction.java
@@ -0,0 +1,36 @@
+package src;
+
+/**
+ * Class for the Subtraction instructions of the SML interpreter
+ * Same as "Add" instruction but with the subtraction...
+ * @author Annabel Jump
+ */
+public class SubInstruction extends Instruction {
+
+    private int result;
+    private int op1;
+    private int op2;
+
+    public SubInstruction(String label, String op) {
+        super(label, op);
+    }
+
+    public SubInstruction(String label, int result, int op1, int op2) {
+        this(label, "sub");
+        this.result = result;
+        this.op1 = op1;
+        this.op2 = op2;
+    }
+
+    @Override
+    public void execute(Machine m) {
+        int value1 = m.getRegisters().getRegister(op1);
+        int value2 = m.getRegisters().getRegister(op2);
+        m.getRegisters().setRegister(result, value1 - value2);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + op1 + " + " + op2 + " to " + result;
+    }
+}
---
+++ cw-one/testSub.sml
@@ -0,0 +1,4 @@
+L0 lin 0 2
+L1 lin 1 1
+L2 sub 2 0 1
+L3 add 3 0 2

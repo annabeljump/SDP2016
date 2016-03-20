3139cf464a44f3b43c990a5cb5834311c0e70744
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-03-20 13:56:25 +0000

    CW1 - amended filepath for my Linux system - ran program, Add instruction works

--- cw-one/src/Translator.java
+++ cw-one/src/Translator.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  */
 public class Translator {
 
-    private static final String PATH = "/home/annabel/Documents/SDP/AJ Portfolio/SDP2016/cw-one/src/";
+    private static final String PATH = "/home/annabel/Documents/SDP/AJPortfolio/SDP2016/cw-one/";
     // word + line is the part of the current line that's not yet processed
     // word has no whitespace
     // If word and line are not empty, line begins with whitespace

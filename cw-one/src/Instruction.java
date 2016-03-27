83abd8354ea6d692863921e9e87bdd46270fc539
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-03-27 15:12:01 +0100

    CW1 - changed Instruction back - not entirely sure why I changed it in the first place but it made sense at the time - does not make sense any more.

--- cw-one/src/Instruction.java
+++ cw-one/src/Instruction.java
@@ -9,21 +9,16 @@ package src;
 public abstract class Instruction {
     protected String label;
     protected String opcode;
-    protected int operator1;
-    protected int operator2;
-    protected int register;
 
 
     // Constructor: an instruction with label l and opcode op
     // (op must be an operation of the language)
     //*****now takes all possible arguments for instructions*****
 
-    public Instruction(String l, String op, int r, int s1, int s2) {
+    public Instruction(String l, String op) {
         this.label = l;
         this.opcode = op;
-        this.register = r;
-        this.operator1 = s1;
-        this.operator2 = s2;
+
     }
 
     // = the representation "label: opcode" of this Instruction

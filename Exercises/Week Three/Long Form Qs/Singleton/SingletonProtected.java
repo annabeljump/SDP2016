17fa5114cd2a99b7a713bcf9c0dc1c7e1037217b
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-03-31 23:28:01 +0100

    Week3 - Singleton - description edited

--- Exercises/Week Three/Long Form Qs/Singleton/SingletonProtected.java
+++ Exercises/Week Three/Long Form Qs/Singleton/SingletonProtected.java
@@ -1,5 +1,7 @@
 /**
  * Created for Singleton Long Form Question from Week 3
+ * Attempt at non-serializable, non-cloneable, not broken by reflection or multiple class loaders
+ * if possible
  * @author Annabel Jump
  */
 

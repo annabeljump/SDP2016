2dbfd9dd7eea381be6e5b53dccd013fe9cc22b3f
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-03-31 23:41:08 +0100

    Week3 - Singleton - serializable borking fixed

--- Exercises/Week Three/Long Form Qs/Singleton/SingletonProtected.java
+++ Exercises/Week Three/Long Form Qs/Singleton/SingletonProtected.java
@@ -1,11 +1,36 @@
+import java.io.Serializable;
+
 /**
  * Created for Singleton Long Form Question from Week 3
- * Attempt at non-serializable, non-cloneable, not broken by reflection or multiple class loaders
+ * Attempt at non-borking Singleton
  * if possible
  * @author Annabel Jump
  */
 
-public class SingletonProtected {
+public class SingletonProtected implements Serializable {
 
+    private static final long serialVersionID = 68L;
+
+    private SingletonProtected(){}
+
+    //this prevents multiple instances being generated in the Serializable case
+    protected Object readResolve() {
+        return getInstance();
+    }
+
+    public static SingletonProtected getInstance(){
+        //First check
+        if(object == null)
+            //Second check - synchronized only called once and only if necessary
+            synchronized (SingletonProtected.class) {
+                if (object == null){
+                    object = new SingletonProtected();
+                }
+            }
+        return object;
+    }
+
+    //Must be volatile or other threads can see partially created instances
+    private volatile static SingletonProtected object;
 
 }

f7f7ffdb0cf6c375261e5c8169b1ba09b4c72621
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-03 00:49:23 +0100

    Week3 - Decorator - forgot a topping

---
+++ Exercises/Week Three/Long Form Qs/Decorator/RomaTomatoes.java
@@ -0,0 +1,9 @@
+/**
+ * Created for pizzaDecorator
+ * @author Annabel Jump
+ */
+public class RomaTomatoes extends PizzaDecorator {
+    public RomaTomatoes(Pizza decoratedPizza) {
+        super(decoratedPizza);
+    }
+}

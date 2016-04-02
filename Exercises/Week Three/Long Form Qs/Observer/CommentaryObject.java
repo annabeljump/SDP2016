d60ce4a4757e72cd797b4253997a481be0af6fd7
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-02 22:48:51 +0100

    Week3 - Observer - working on CommentaryObject. some methods sorted, others guessed at.

--- Exercises/Week Three/Long Form Qs/Observer/CommentaryObject.java
+++ Exercises/Week Three/Long Form Qs/Observer/CommentaryObject.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class CommentaryObject implements Subject {
     private ArrayList<Observer> subList;
     private String matchDetails;
+    private String playByPlay;
 
     public CommentaryObject(ArrayList<Observer> list, String str){
         this.subList = list;
@@ -15,21 +16,28 @@ public class CommentaryObject implements Subject {
 
     @Override
     public void subscribeObserver(Observer observer) {
-
+        subList.add(observer);
     }
 
     @Override
     public void unSubscribeObserver(Observer observer) {
 
+        //need to test to see if removes the correct observer - may need to use .equals
+        subList.remove(observer);
     }
 
+    //need to update playByPlay or should this be matchDetails??
     @Override
     public void notifyObservers() {
+        for(Observer observer : subList){
+            observer.update(playByPlay);
+        }
 
     }
 
+    //not entirely sure what this method is at this point. This is a guess.
     @Override
     public String subjectDetails() {
-        return null;
+        return matchDetails;
     }
 }

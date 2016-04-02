01d0f41c54cd5565ef5aa51870c2171af494779c
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-02 22:15:35 +0100

    Week3 - Observer - added fields and constructors to SMSUsers and CommentaryObject - classes must implement interfaces to do so

--- Exercises/Week Three/Long Form Qs/Observer/CommentaryObject.java
+++ Exercises/Week Three/Long Form Qs/Observer/CommentaryObject.java
@@ -1,7 +1,35 @@
+import java.util.ArrayList;
+
 /**
  * Created for Observer pattern question
  * @author Annabel Jump
  */
-public class CommentaryObject {
+public class CommentaryObject implements Subject {
+    private ArrayList<Observer> subList;
+    private String matchDetails;
 
+    public CommentaryObject(ArrayList<Observer> list, String str){
+        this.subList = list;
+        this.matchDetails = str;
+    }
+
+    @Override
+    public void subscribeObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void unSubscribeObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
+
+    @Override
+    public String subjectDetails() {
+        return null;
+    }
 }
--- Exercises/Week Three/Long Form Qs/Observer/SMSUsers.java
+++ Exercises/Week Three/Long Form Qs/Observer/SMSUsers.java
@@ -2,6 +2,27 @@
  * Created for Observer pattern exercise
  * @author Annabel Jump
  */
-public class SMSUsers {
+public class SMSUsers implements Observer {
+    private String name;
+    private Subject match;
 
+    public SMSUsers(Subject subject, String str){
+        this.match = subject;
+        this.name = str;
+    }
+
+    @Override
+    public void update(String desc) {
+
+    }
+
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unSubscribe() {
+
+    }
 }

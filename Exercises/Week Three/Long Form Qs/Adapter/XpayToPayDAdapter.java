04f3e0ab32ce63c062951d1bcb959109cccbb980
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-02 18:54:43 +0100

    Week3 - Adapter - after thought, adapter should implement payd otherwise what is the point!

--- Exercises/Week Three/Long Form Qs/Adapter/XpayToPayDAdapter.java
+++ Exercises/Week Three/Long Form Qs/Adapter/XpayToPayDAdapter.java
@@ -2,9 +2,58 @@
  * Long form Question 3: Adapter design pattern
  * @author Annabel Jump
  */
-public class XpayToPayDAdapter {
+public class XpayToPayDAdapter implements PayD {
 
     private Xpay fromThis = new XpayImpl();
-    private PayD toThis = new PayDImpl();
 
+
+    @Override
+    public String getCustCardNo() {
+        return null;
+    }
+
+    @Override
+    public void setCustCardNo(String custCardNo) {
+
+    }
+
+    @Override
+    public String getCardOwnerName() {
+        return null;
+    }
+
+    @Override
+    public void setCardOwnerName(String cardOwnerName) {
+
+    }
+
+    @Override
+    public String getCardExpMonthDate() {
+        return null;
+    }
+
+    @Override
+    public void setCardExpMonthDate(String cardExpMonthDate) {
+
+    }
+
+    @Override
+    public Integer getCVVNo() {
+        return null;
+    }
+
+    @Override
+    public void setCVVNo(Integer cVVNo) {
+
+    }
+
+    @Override
+    public Double getTotalAmount() {
+        return null;
+    }
+
+    @Override
+    public void setTotalAmount(Double totalAmount) {
+
+    }
 }

eb378071aee6570fb0a01d1c2334ee0b8a0f906f
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-02 19:08:52 +0100

    Week3 - Adapter - after thought, adapter should implement payd otherwise what is the point!

--- Exercises/Week Three/Long Form Qs/Adapter/XpayToPayDAdapter.java
+++ Exercises/Week Three/Long Form Qs/Adapter/XpayToPayDAdapter.java
@@ -6,54 +6,59 @@ public class XpayToPayDAdapter implements PayD {
 
     private Xpay fromThis = new XpayImpl();
 
+    private String custCardNo;
+    private String cardOwnerName;
+    private String cardExpMonthDate;
+    private Integer cVVNo;
+    private Double totalAmount;
 
     @Override
     public String getCustCardNo() {
-        return null;
+        return this.custCardNo;
     }
 
     @Override
     public void setCustCardNo(String custCardNo) {
-
+        this.custCardNo = custCardNo;
     }
 
     @Override
     public String getCardOwnerName() {
-        return null;
+        return this.cardOwnerName;
     }
 
     @Override
     public void setCardOwnerName(String cardOwnerName) {
-
+        this.cardOwnerName = cardOwnerName;
     }
 
     @Override
     public String getCardExpMonthDate() {
-        return null;
+        return cardExpMonthDate;
     }
 
     @Override
     public void setCardExpMonthDate(String cardExpMonthDate) {
-
+        this.cardExpMonthDate = cardExpMonthDate;
     }
 
     @Override
     public Integer getCVVNo() {
-        return null;
+        return this.cVVNo;
     }
 
     @Override
     public void setCVVNo(Integer cVVNo) {
-
+        this.cVVNo = cVVNo;
     }
 
     @Override
     public Double getTotalAmount() {
-        return null;
+        return this.totalAmount;
     }
 
     @Override
     public void setTotalAmount(Double totalAmount) {
-
+        this.totalAmount = totalAmount;
     }
 }

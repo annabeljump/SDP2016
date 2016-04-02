7b1ef35654c548e6550a3f1e883727b1f8f1dc62
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-02 23:39:37 +0100

    Week3 - Observer - SMSUsers completed. Will now run TestObserver to see if works

--- Exercises/Week Three/Long Form Qs/Observer/SMSUsers.java
+++ Exercises/Week Three/Long Form Qs/Observer/SMSUsers.java
@@ -6,6 +6,8 @@ public class SMSUsers implements Observer {
     private String name;
     private Subject match;
 
+    private Observer user = new SMSUsers(match, name);
+
     public SMSUsers(Subject subject, String str){
         this.match = subject;
         this.name = str;
@@ -13,16 +15,16 @@ public class SMSUsers implements Observer {
 
     @Override
     public void update(String desc) {
-
+        System.out.println(name + ", New Match Update for " + match.subjectDetails() + ": /n" + desc);
     }
 
     @Override
     public void subscribe() {
-
+        match.subscribeObserver(user);
     }
 
     @Override
     public void unSubscribe() {
-
+        match.unSubscribeObserver(user);
     }
 }

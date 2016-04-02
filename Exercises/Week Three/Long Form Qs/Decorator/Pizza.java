a161500886769232317c7c37884bfa6e86594f63
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-03 00:13:22 +0100

    Week3 - Decorator - initialised with classes from exercise.

---
+++ Exercises/Week Three/Long Form Qs/Decorator/Pizza.java
@@ -0,0 +1,12 @@
+/**
+ * Copied from the exercise sheet
+ * Because who DOESN'T like pizza??
+ * @author not Annabel
+ */
+
+public interface Pizza {
+
+    String getDesc();
+
+    double getPrice();
+}

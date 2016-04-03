dbb91fe3191fc432a0076cd03d796c467a56cd1d
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-03 12:19:19 +0100

    Week 4 - done Expressions. will do worksheet to check them

--- Exercises/Week Four/expressions.scala
+++ Exercises/Week Four/expressions.scala
@@ -6,9 +6,21 @@
 class expressions {
 
   var isSunny = true
+  var isPartlyCloudy = false
   var temperature = 75
 
   //1.
   if (isSunny && temperature > 80) true
 
+  //2.
+  if ((isSunny || isPartlyCloudy) && temperature > 80) true
+
+  //3.
+  if ((isSunny || isPartlyCloudy) && (temperature > 80 || temperature < 20)) true
+
+  //4.
+  var tempCelsius = (temperature - 32) * (5/9)
+
+  //5.
+  var fahrenheit451 = (tempCelsius * (9/5)) + 32
 }

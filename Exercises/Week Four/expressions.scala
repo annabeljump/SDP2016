9a5ef5539c99d8cfa0cc087d3501ff180e015bc4
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-03 12:25:02 +0100

    Week 4 - avoided Integer maths, added false return to if expressions (as per worksheet)

--- Exercises/Week Four/expressions.scala
+++ Exercises/Week Four/expressions.scala
@@ -7,19 +7,22 @@ class expressions {
 
   var isSunny = true
   var isPartlyCloudy = false
-  var temperature = 75
+  var temperature = 15
 
   //1.
   if (isSunny && temperature > 80) true
+  else false
 
   //2.
   if ((isSunny || isPartlyCloudy) && temperature > 80) true
+  else false
 
   //3.
   if ((isSunny || isPartlyCloudy) && (temperature > 80 || temperature < 20)) true
+  else false
 
   //4.
-  var tempCelsius = (temperature - 32) * (5/9)
+  var tempCelsius = (temperature - 32) * (5f/9)
 
   //5.
   var fahrenheit451 = (tempCelsius * (9/5)) + 32
---
+++ Exercises/Week Four/expressionsWorksheet.sc
@@ -0,0 +1,21 @@
+var isSunny = true
+var isPartlyCloudy = false
+var temperature = 15
+
+//1.
+if (isSunny && temperature > 80) true
+else false
+
+//2.
+if ((isSunny || isPartlyCloudy) && temperature > 80) true
+else false
+
+//3.
+if ((isSunny || isPartlyCloudy) && (temperature > 80 || temperature < 20)) true
+else false
+
+//4.
+var tempCelsius = (temperature - 32) * (5f/9)
+
+//5.
+var fahrenheit451 = (tempCelsius * (9/5)) + 32

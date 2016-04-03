b3ee87a853eee4cb34836e4092f9c5afe9174aed
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-03 11:55:49 +0100

    Week 4 - expressions 1 done, renamed first section of exercises

---
+++ Exercises/Week Four/expressions.scala
@@ -0,0 +1,14 @@
+/**
+  * Created for week 4 worksheet
+  * "Expressions" exercises
+  * @author Annabel Jump
+  */
+class expressions {
+
+  var isSunny = true
+  var temperature = 75
+
+  //1.
+  if (isSunny && temperature > 80) true
+
+}

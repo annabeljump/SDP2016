f790a267b0126fa8ffc60d6936d778f6635be1e7
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-09 21:03:06 +0100

    Week 6 - Q3 - figured out how to take arbitrary number of objects. Now to figure out familySize()

---
+++ Exercises/WeekSix/TestArgs.scala
@@ -0,0 +1,19 @@
+/**
+  * Created for week 6 questions 3 and 4
+  * @author Annabel
+  */
+import AtomicTest._
+
+object TestArgs extends App {
+
+  class Family(val member: String*) {
+    def familySize(): Int = {
+
+    }
+  }
+
+  val family1 = new Family("Mum", "Dad", "Sally", "Dick")
+  family1.familySize() is 4
+  val family2 = new Family("Dad", "Mom", "Harry")
+  family2.familySize() is 3
+}

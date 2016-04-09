2daaf5287291f51c8f55887edf8169fab96fd693
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-09 14:59:43 +0100

    Week 5 - q4 added NYC parsers

---
+++ Exercises/Week Five/Question4AbstractFactory/NYCErrorXMLParser.java
@@ -0,0 +1,12 @@
+package Question4AbstractFactory;
+
+/**
+ * Created for abstract factory pattern
+ * @author Annabel
+ */
+public class NYCErrorXMLParser implements XMLParser {
+    @Override
+    public String parse() {
+        return null;
+    }
+}
---
+++ Exercises/Week Five/Question4AbstractFactory/NYCFeedbackXML.java
@@ -0,0 +1,12 @@
+package Question4AbstractFactory;
+
+/**
+ * Created for abstract factory pattern
+ * @author Annabel
+ */
+public class NYCFeedbackXML implements XMLParser {
+    @Override
+    public String parse() {
+        return null;
+    }
+}

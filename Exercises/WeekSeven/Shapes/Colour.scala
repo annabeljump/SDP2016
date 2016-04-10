5280d9a64a1828c963d8037b699aba9e5282906b
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-10 15:01:34 +0100

    Week 7 - Q5  - added means to create new colours

--- Exercises/WeekSeven/Shapes/Colour.scala
+++ Exercises/WeekSeven/Shapes/Colour.scala
@@ -11,3 +11,23 @@ trait Colour {
   def bright(): String
 
 }
+
+object Colour {
+
+  val red: Int = _
+  val green: Int = _
+  val blue: Int = _
+
+  def customColour(r: Int, g: Int, b: Int): Colour = {
+    new Colour {
+      override val red = r
+      override val green = g
+      override val blue = b
+
+      override def bright(): String = {
+        if ( red < 50 && green < 50 && blue < 50) "light"
+        else "dark"
+      }
+    }
+  }
+}

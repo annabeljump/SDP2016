2c3b9dd6b0bcc43ba71b131104b630722e332e4e
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-10 13:03:39 +0100

    Week 7 - Q5 - square done

--- Exercises/WeekSeven/Shapes/Square.scala
+++ Exercises/WeekSeven/Shapes/Square.scala
@@ -3,10 +3,19 @@ package Shapes
 /**
   * Created for week 7 Q5
   */
-class Square extends Shape {
-  override def sides(): Unit = ???
+class Square(edge: Int = 4, length: Int = 4) extends Shape {
 
-  override def perimeter(): Unit = ???
+  val numberSides = 4
 
-  override def area(): Unit = ???
+  override def sides(): Int = {
+    numberSides
+  }
+
+  override def perimeter(): Int = {
+    edge * numberSides
+  }
+
+  override def area(): Int = {
+    edge * edge
+  }
 }

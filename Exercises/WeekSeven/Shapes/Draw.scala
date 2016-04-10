5d7b738d506c5fc987a3c3ae70f8d30b4f786f29
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-10 14:35:05 +0100

    Week 7 - Q5 - added calls to Draw, it runs

--- Exercises/WeekSeven/Shapes/Draw.scala
+++ Exercises/WeekSeven/Shapes/Draw.scala
@@ -10,9 +10,11 @@ object Draw extends App {
   def apply(sh: Shape) = sh match {
       //fixed!
       case Circle(radius) => "A circle of radius " + radius + "cm"
-      //case Rectangle(width, height) => "A rectangle of width " + width + "cm and height " + height + "cm"
+      case Rectangle(width, height) => "A rectangle of width " + width + "cm and height " + height + "cm"
       case Square(edge) => "A square with sides of " + edge + "cm"
     }
 
-
+  println(Draw(Circle(10)))
+  println(Draw(Square(4)))
+  println(Draw(Rectangle(3,6)))
 }

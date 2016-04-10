5bff5fdc96a005a0b6516e6c84e5ae59723be9ff
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-10 14:56:08 +0100

    Week 7 - Q5  - added pink

--- Exercises/WeekSeven/Shapes/Pink.scala
+++ Exercises/WeekSeven/Shapes/Pink.scala
@@ -2,15 +2,14 @@
   * Created for week 7 question 5
   * @author Annabel
   */
-case class Yellow() extends Colour {
+case class Pink() extends Colour {
 
-  override val red = 30
-  override val green = 45
+  override val red = 10
+  override val green = 0
   override val blue = 0
 
   override def bright(): String = {
     if ( red < 50 && green < 50 && blue < 50) "light"
     else "dark"
   }
-
 }

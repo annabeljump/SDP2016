39f74b1c1807569561e74bda900eb3ab42dd6cbe
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-10 13:06:06 +0100

    Week 7 - Q5 - rectangle done - square had unnecessary parameter

--- Exercises/WeekSeven/Shapes/Rectangle.scala
+++ Exercises/WeekSeven/Shapes/Rectangle.scala
@@ -4,11 +4,19 @@ package Shapes
   * Created for week 7 Q5
   * @author Annabel
   */
-class Rectangle extends Shape {
+class Rectangle(long: Int = 4, short: Int = 2) extends Shape {
 
-  override def sides(): Unit = ???
+  val numberSides = 4
 
-  override def perimeter(): Unit = ???
+  override def sides(): Int = {
+    numberSides
+  }
 
-  override def area(): Unit = ???
+  override def perimeter(): Int = {
+    (long * 2) + (short * 2)
+  }
+
+  override def area(): Int = {
+    long * short
+  }
 }
